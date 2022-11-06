379a1592d7a47513c15ac9ab70c69beead12def2
Adrian Webb <adrian@webb.sh>
2022-11-06 04:24:03 -0500

    Updating output variables.

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,24 @@
 
-output "instance_id" {
-  value = google_compute_instance.zimagi.instance_id
+output "zimagi_name" {
+  value = var.instance_name
 }
 
-output "external_ip" {
+output "zimagi_host" {
   value = google_compute_instance.zimagi.network_interface.0.access_config.0.nat_ip
 }
-output "internal_ip" {
-  value = google_compute_instance.zimagi.network_interface.0.network_ip
+output "zimagi_command_port" {
+  value = var.command_api_port
+}
+output "zimagi_data_port" {
+  value = var.data_api_port
 }
 
-output "ssh_access_location" {
-    value = "${var.instance_user}@${google_compute_instance.zimagi.network_interface.0.access_config.0.nat_ip}"
+output "ssh_host" {
+  value = google_compute_instance.zimagi.network_interface.0.access_config.0.nat_ip
 }
-
-output "zimagi_command_api" {
-    value = "https://${google_compute_instance.zimagi.network_interface.0.access_config.0.nat_ip}:${var.command_api_port}/"
+output "ssh_port" {
+  value = 22
 }
-output "zimagi_data_api" {
-    value = "https://${google_compute_instance.zimagi.network_interface.0.access_config.0.nat_ip}:${var.data_api_port}/"
+output "ssh_user" {
+  value = var.instance_user
 }
